34d78065101534caae0ffd0c99fdd9870ec5e062
Conroy Whitney <conroy.whitney@gmail.com>
2017-09-10 09:04:19 -0800

    Add `eslint` via npm / yarn

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    sourceType: "module"
+  },
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: "prettier",
+  plugins: [],
+  rules: {}
+};
