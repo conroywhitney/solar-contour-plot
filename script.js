a271eba740d6881481d36bc38a607a1d28eed0ab
Conroy Whitney <conroy.whitney@gmail.com>
2017-09-10 11:45:25 -0800

    Ensure graph does not overlay footer

--- script.js
+++ script.js
@@ -132,7 +132,7 @@ var solarContour = new Vue({
         },
         {
           x: [90, 135, 180, 225, 270],
-          y: [3.5, 3.5, 3.5, 3.5, 3.5],
+          y: [5, 5, 5, 5, 5],
           mode: "text",
           text: ["E", "SE", "S", "SW", "W"],
           textposition: "bottom",
